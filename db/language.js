95201cde6b5b7292be7d12eef48958db71785ada
TempVoice <exotherm.one@gmail.com>
2022-06-17 14:35:44 +0200

    Init

--- db/language.js
+++ db/language.js
@@ -3,8 +3,9 @@ const { Schema, model } = require('mongoose')
 const languageSchema = new Schema(
 	{
 		name: { type: String, index: true },
-		lang: { type: String, index: true, default: 'de' },
+		lang: { type: String, index: true, default: 'en' },
 		text: { type: String, default: 'Missing Text' },
+		discord_lang: { type: String, default: 'en-US' },
 	},
 	{ id: false, timestamps: { createdAt: true, updatedAt: true } }
 )
