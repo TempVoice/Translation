17240f233278ee14b6e2fb9c5f9c636c5c261224
TempVoice <exotherm.one@gmail.com>
2022-06-18 13:12:50 +0200

    Updated

--- scripts/langdb/import.js
+++ scripts/langdb/import.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
 const { Languages } = require('../../db')
 
-let arr = ["./language_nl.json", "./language_ae.json", "./language_de.json", "./language_en.json", "./language_es.json", "./language_fr.json", "./language_id.json", "./language_il.json", "./language_it.json", "./language_ko.json", "./language_lt.json", "./language_pl.json", "./language_pt.json", "./language_ru.json", "./language_tr.json"];
-//let arr = ["./language_nl.json", "./language_ae.json", "./language_de.json", "./language_en.json",];
+//let arr = ["./language_nl.json", "./language_ae.json", "./language_de.json", "./language_en.json", "./language_es.json", "./language_fr.json", "./language_id.json", "./language_il.json", "./language_it.json", "./language_ko.json", "./language_lt.json", "./language_pl.json", "./language_pt.json", "./language_ru.json", "./language_tr.json"];
+let arr = ["./language_en.json"];
 for (let i = 0; i < arr.length; i++) {
 	const TransFile = require(arr[i]);
 	const { lang, discord_lang } = TransFile[0]
