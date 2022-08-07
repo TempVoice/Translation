6e0420516afeb6486dac596d9d1125313eb329bc
TempVoice <exotherm.one@gmail.com>
2022-08-07 14:41:07 +0200

    Updated

--- db/language.js
+++ db/language.js
@@ -25,21 +25,21 @@ languageSchema.static({
 		let text
 		lang = serverdb.lang
 
-        switch(serverdb.color) {
-            case "18af92":
-                emoji = ["", "<:TempVoice_Success_Green:950434120738148422>", "<:TempVoice_Error_Green:950434120729763961>", "<:TempVoice_Trust_Green:950434120767524894>", "<:TempVoice_Help_Green:950434120822054922>", "<:TempVoice_List_Green:951205543853318245>", "<:TempVoice_Premium:932755043235561513>"]
+        switch (serverdb.color) {
+            case '18af92':
+                emoji = ['', '<:TempVoice_Success_Green:950434120738148422>', '<:TempVoice_Error_Green:950434120729763961>', '<:TempVoice_Trust_Green:950434120767524894>', '<:TempVoice_Help_Green:950434120822054922>', '<:TempVoice_List_Green:951205543853318245>', '<:TempVoice_Premium:932755043235561513>']
                 break;
-            case "7789e1":
-                emoji = ["", "<:TempVoice_Success_Blue:950433575453474868>", "<:TempVoice_Error_Blue:950433575835148308>", "<:TempVoice_Trust_Blue:950433575830966282>", "<:TempVoice_Help_Blue:951205704046358608>", "<:TempVoice_List_Blue:951205704214138940>", "<:TempVoice_Premium:932755043235561513>"]
+            case '7789e1':
+                emoji = ['', '<:TempVoice_Success_Blue:950433575453474868>', '<:TempVoice_Error_Blue:950433575835148308>', '<:TempVoice_Trust_Blue:950433575830966282>', '<:TempVoice_Help_Blue:951205704046358608>', '<:TempVoice_List_Blue:951205704214138940>', '<:TempVoice_Premium:932755043235561513>']
                 break;
-            case "a77fdb":
-                emoji = ["", "<:TempVoice_Success_Purple:950484912417427456>", "<:TempVoice_Error_Purple:950484912144777277>", "<:TempVoice_Trust_Purple:950484912610365481>", "<:TempVoice_Help_Purple:951206011170070548> ", "<:TempVoice_List_Purple:951206011358810112>", "<:TempVoice_Premium:932755043235561513>"]
+            case 'a77fdb':
+                emoji = ['', '<:TempVoice_Success_Purple:950484912417427456>', '<:TempVoice_Error_Purple:950484912144777277>', '<:TempVoice_Trust_Purple:950484912610365481>', '<:TempVoice_Help_Purple:951206011170070548> ', '<:TempVoice_List_Purple:951206011358810112>', '<:TempVoice_Premium:932755043235561513>']
                 break;
-            case "d77620":
-                emoji = ["", "<:TempVoice_Success_Orange:950434327810940948>", "<:TempVoice_Error_Orange:950434327706083388>", "<:TempVoice_Trust_Orange:950434327756406844>", "<:TempVoice_Help_Orange:951206629393698826>", "<:TempVoice_List_Orange:951206629469220874>", "<:TempVoice_Premium:932755043235561513>"]
+            case 'd77620':
+                emoji = ['', '<:TempVoice_Success_Orange:950434327810940948>', '<:TempVoice_Error_Orange:950434327706083388>', '<:TempVoice_Trust_Orange:950434327756406844>', '<:TempVoice_Help_Orange:951206629393698826>', '<:TempVoice_List_Orange:951206629469220874>', '<:TempVoice_Premium:932755043235561513>']
                 break;
-            case "e1b70e":
-                emoji = ["", "<:TempVoice_Success_Yellow:950484945095229450>", "<:TempVoice_Error_Yellow:950484944713580575>", "<:TempVoice_Trust_Yellow:950484945187536906>", "<:TempVoice_Help_Yellow:951205869712986192>", "<:TempVoice_List_Yellow:951205869893337188>", "<:TempVoice_Premium:932755043235561513>"]
+            case 'e1b70e':
+                emoji = ['', '<:TempVoice_Success_Yellow:950484945095229450>', '<:TempVoice_Error_Yellow:950484944713580575>', '<:TempVoice_Trust_Yellow:950484945187536906>', '<:TempVoice_Help_Yellow:951205869712986192>', '<:TempVoice_List_Yellow:951205869893337188>', '<:TempVoice_Premium:932755043235561513>']
                 break;
         }
 
