f90c815b92c8ea182626c53d1c80403711b70a64
Dad-Ju <Justin.Bedrich@gmail.com>
2022-10-21 12:07:47 +0200

    update for external DB

--- db/index.js
+++ db/index.js
@@ -1,7 +1,15 @@
 /* eslint-disable global-require */
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.dbstring)
+mongoose.connect(process.env.dbstring, {
+	authMechanism: 'DEFAULT',
+	dbName: process.env.dbName,
+	auth: {
+		username: process.env.dbuser,
+		password: process.env.dbpwd,
+	},
+})
+
 
 module.exports = {
 	Languages: require('./language'),
