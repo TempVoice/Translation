e5b837bb975e4d7bf2812719dfe26e9cdd904c40
TempVoice <exotherm.one@gmail.com>
2023-01-28 22:11:37 +0100

    Updated

--- scripts/langdb/import.js
+++ scripts/langdb/import.js
@@ -2,28 +2,28 @@ require('dotenv').config()
 const { Languages } = require('../../db')
 
 let arr = [
-	'./language_cs.json',
-	'./language_vi.json',
-	'./language_sv.json',
-	'./language_el.json',
-	'./language_zh-TW.json',
-	'./language_uk.json',
-	'./language_hu.json',
-	'./language_nl.json',
 	'./language_ae.json',
-	'./language_de.json',
-	'./language_en.json',
-	'./language_es.json',
-	'./language_fr.json',
-	'./language_id.json',
+    './language_cs.json',
+    './language_de.json',
+    './language_el.json',
+    './language_en.json',
+    './language_es.json',
+    './language_fr.json',
+    './language_hu.json',
+    './language_id.json',
 	'./language_il.json',
 	'./language_it.json',
 	'./language_ko.json',
 	'./language_lt.json',
+	'./language_nl.json',
 	'./language_pl.json',
 	'./language_pt.json',
 	'./language_ru.json',
-	'./language_tr.json'
+	'./language_sv.json',
+	'./language_tr.json',
+	'./language_uk.json',
+	'./language_vi.json',
+	'./language_zh-TW.json',
 ];
 
 // let arr = ['./language_vi.json'];
