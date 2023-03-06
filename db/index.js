db618c54bc457bf3e247bb37267fc16c1e8fea03
TempVoice <exotherm.one@gmail.com>
2023-03-06 12:56:48 +0100

    Updated de and en

--- db/index.js
+++ db/index.js
@@ -1,12 +1,12 @@
 /* eslint-disable global-require */
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.dbstring, {
+mongoose.connect(process.env.DB_STRING, {
 	authMechanism: 'DEFAULT',
-	dbName: process.env.dbName,
+	dbName: process.env.DB_NAME,
 	auth: {
-		username: process.env.dbuser,
-		password: process.env.dbpwd,
+		username: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
 	},
 })
 
