f1346879789f88151a7ca25421386fc7c342012a
TempVoice <exotherm.one@gmail.com>
2023-03-25 17:35:15 +0100

    Rewrite

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const { Languages } = require('./database')
+
+let arr = ['ae', 'cs', 'de', 'el', 'en', 'es', 'fr', 'hu', 'id', 'il', 'it', 'ko', 'lt', 'nl', 'pl', 'pt', 'ru', 'sv-SE', 'tr', 'uk', 'vi', 'zh-TW', 'ja'];
+
+for (let i = 0; i < arr.length; i++) {
+	const TransFile = require('./locales/' + arr[i] + '/translation.json');
+	const { lang, discord_lang } = TransFile[0]
+
+
+	Object.entries(TransFile[1]).forEach(async ([name, text]) => {
+		let LangItem = await Languages.findOne({ name, lang })
+
+		if (LangItem === null) LangItem = new Languages()
+
+		LangItem.overwrite({
+			lang, name, text, discord_lang
+		})
+
+		console.log(`Adding => ${lang} : ${name}`)
+		await LangItem.save()
+		console.log(`Added! ${lang} : ${name}`)
+	})
+}
