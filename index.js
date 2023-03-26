4ad9f61f043ae44b316ac5dec0cd7df9b86b6409
TempVoice <exotherm.one@gmail.com>
2023-03-26 15:30:18 +0200

    npx prettier --write .

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 require('dotenv').config()
 const { Languages } = require('./database')
 
-let arr = ['ae', 'cs', 'de', 'el', 'en', 'es', 'fr', 'hu', 'id', 'il', 'it', 'ko', 'lt', 'nl', 'pl', 'pt', 'ru', 'sv-SE', 'tr', 'uk', 'vi', 'zh-TW', 'ja'];
+let arr = [
+	'ae',
+	'cs',
+	'de',
+	'el',
+	'en',
+	'es',
+	'fr',
+	'hu',
+	'id',
+	'il',
+	'it',
+	'ko',
+	'lt',
+	'nl',
+	'pl',
+	'pt',
+	'ru',
+	'sv-SE',
+	'tr',
+	'uk',
+	'vi',
+	'zh-TW',
+	'ja',
+]
 
 for (let i = 0; i < arr.length; i++) {
-	const TransFile = require('./locales/' + arr[i] + '/translation.json');
+	const TransFile = require('./locales/' + arr[i] + '/translation.json')
 	const { lang, discord_lang } = TransFile[0]
-
 
 	Object.entries(TransFile[1]).forEach(async ([name, text]) => {
 		let LangItem = await Languages.findOne({ name, lang })
@@ -14,7 +37,10 @@ for (let i = 0; i < arr.length; i++) {
 		if (LangItem === null) LangItem = new Languages()
 
 		LangItem.overwrite({
-			lang, name, text, discord_lang
+			lang,
+			name,
+			text,
+			discord_lang,
 		})
 
 		console.log(`Adding => ${lang} : ${name}`)
